d850677ef32dcf2a052ed2a55ce1dffbe0bc68cc
barahijawi <34750743+barahijawi@users.noreply.github.com>
2017-12-21 22:56:11 +0400

    cleaning code

--- public/index.php
+++ public/index.php
@@ -1,10 +1,7 @@
 <?php
-// Composer autoload
 define('BASE_URL', str_replace(basename(__dir__ ), '', __dir__ ));
 require BASE_URL . '/public/autoload.php';
 //use \Triptip as Triptip;
-//require(BASE_URL . '/vendor/autoload.php');
-//use \triptip\src\app\core\Trip;
 try {
     $trip = new Trip(provider::jsonProvider());
     // Sort them
